24b6c9a15988181daab295f68726a7e88a625179
tianjian-sky <532861047@qq.com>
2018-03-16 23:55:40 +0800

    redux-promise

--- src/modules/login/index.js
+++ src/modules/login/index.js
@@ -29,19 +29,45 @@ export default class Login extends React.Component {
             //2. 异步action
             //2.1 操作开始action
             //2.2 得到异步响应action
-            // store只支持dispatch中传对象，因此需用到 redux-thunk 中间件进行改造
-            let thunk = () => {
+            //1. store只支持dispatch中传对象，因此需用到 redux-thunk 中间件进行改造
+            // let thunk = function () {
+            //     return function (dispatch, getState) {
+            //         dispatch({type: 'SET_FETCHING_TRUE'})
+            //         let req = new Promise((resolve,reject) => {
+            //             setTimeout(() => {
+            //                 resolve()
+            //             }, 3000)
+            //         })
+            //         req.then(() => {
+            //             dispatch({type: 'SET_FETCHING_FALSE'})
+            //         })
+            //     }
+            // }
+            // store.dispatch(thunk())
+            // 2 使用redux-promise 使dispatch方法中支持primise
+            // 2.1 thunk 的返回值是一个promise
+            let thunkP = () => {
                 store.dispatch({type: 'SET_FETCHING_TRUE'})
-                let req = new Promise((resolve,reject) => {
-                    setTimeout(() => {
-                        resolve()
+                return new Promise(function (rs, rj) {
+                    setTimeout(function () {
+                        rs({type: 'SET_FETCHING_FALSE'})
                     }, 3000)
                 })
-                req.then(() => {
-                    store.dispatch({type: 'SET_FETCHING_FALSE'})
-                })
             }
-            store.dispatch(thunk())
+            // 3.redux-promise
+            // let thunkP = {
+            //     type: 'SET_FETCHING_TRUE',
+            //     payload: new Promise(function (rs, rj) {
+            //         setTimeout(function () {
+            //             rs({type: 'SET_FETCHING_FALSE'})
+            //         }, 3000)
+            //     })
+            // }
+
+
+            store.dispatch(thunkP)
+
+            //4.redux 异步方案选择 https://segmentfault.com/a/1190000007248878
         }
     }
     render () {
--- src/statesMgr/store.js
+++ src/statesMgr/store.js
@@ -1,10 +1,16 @@
 import {reducer} from './reducer'
 import { createStore,applyMiddleware } from 'redux'
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
 import {createLogger} from 'redux-logger';
+// import promiseMiddleware from 'redux-promise-middleware';
 
 const logger = createLogger()
 
-// store只支持dispatch中传对象,直接同步改状态，如果要等异步请求到了后改状态，需用到 redux-thunk 中间件进行改造
+//1. store只支持dispatch中传对象,直接同步改状态，如果要等异步请求到了后改状态，需用到 redux-thunk 中间件进行改造
 //store.dispatch(thunk())
-export const store = createStore(reducer, applyMiddleware(thunk,logger));
+
+// export const store = createStore(reducer, applyMiddleware(thunk,logger));
+
+//2. 使用redux-promise 使dispatch方法中支持primise
+export const store = createStore(reducer, applyMiddleware(promiseMiddleware,logger));
